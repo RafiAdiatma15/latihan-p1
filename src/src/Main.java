90a19fd29a076c245646587b99b954dec7b7327d
Muhammad Rafi Adiatma <muhammadrafiadiatma4@gmail.com>
2026-03-15 12:47:24 +0700

    init comit

---
+++ src/src/Main.java
@@ -0,0 +1,23 @@
+package com.siinfo;
+
+/**
+ * Program sederhana untuk menampilkan data mahasiswa
+ */
+public class Main {
+
+    /**
+     * Method utama program
+     */
+    public static void main(String[] args) {
+
+        String nama = "Adiatma Ganz";
+        String nim = "12345678";
+        String prodi = "Sistem Informasi";
+        String motivasi = "Saya belajar Java untuk meningkatkan kemampuan pemrograman dan membuat aplikasi yang bermanfaat.";
+
+        System.out.println("Nama Lengkap : " + nama);
+        System.out.println("NIM          : " + nim);
+        System.out.println("Program Studi: " + prodi);
+        System.out.println("Motivasi     : " + motivasi);
+    }
+}
